a342b30231bcf89fbb2a950bfca490bfa32717ed
Nibedita Chakraborty <96379756+NiviRocks@users.noreply.github.com>
2022-05-30 22:03:55 +0530

    Update insertion sort.py

--- insertion sort.py
+++ insertion sort.py
@@ -1,47 +1,18 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jan  3 21:08:54 2022
+''' INSERTION SORT '''
 
-@author: LENOVO
-"""
-
-''' INSERTION SORT'''
-
-def insertion_sort(lst): #my code -> CORRECT OUTPUT AND COMPLEXITY
-    itera=0
+def isort(lst): 
     for i in range(1,len(lst)):
-        itera+=1 #count iteration
-        ind=i # ind will get changed as lst[i] value gets placed at its right position
-        j=i-1
-        while j>=0 and lst[j]>lst[ind]: # enter loop only if lst[ind] is in improper position
-            itera+=1
-            lst[ind],lst[j]=lst[j],lst[ind] # swap i,j index values
-            ind-=1 # lst[i] position gets swapped to i-1 = ind-1 pos
-            j-=1
-    print("Sorted List:",lst)
-    return itera
-
-def isort(lst): #actual code
-    itera=0
-    for i in range(1,len(lst)):
-        itera+=1
         key=lst[i]
         j=i-1
         while j>=0 and key<lst[j]:
-            itera+=1
             lst[j+1]=lst[j]
-            #print(lst)
             j-=1
         lst[j+1]=key
-        #print(lst)
-    print("Sorted list:",lst)
-    return itera
+    return lst
 
 lst=list(map(int,input("Enter list of nos.:").split()))
-c=insertion_sort(lst)
-print("Iterations required 1:",c)
-#d=isort(lst)
-#print("Iterations required 2:",d)
+sort_lst=insertion_sort(lst)
+print("Sorted list:",sort_lst)
 
 '''
 WORST CASE:
